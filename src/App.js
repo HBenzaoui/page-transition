cdc049157d50955ef03c1edfcc9380d76e1ab727
Hamza Benzaoui <hamzapdd@gmail.com>
2019-01-07 10:17:10 +0100

    install react-route-dom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import NavBar from './layouts/NavBar';
+import { Router } from 'react-router';
+// import
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        {/* <Home /> */}
-        {/* <About /> */}
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          {/* <Home /> */}
+          {/* <About /> */}
+        </div>
+      </Router>
     );
   }
 }
--- src/layouts/NavBar.js
+++ src/layouts/NavBar.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-dom';
+import Link from 'react-dom';
 
 const NavBar = () => {
   return (
     <div className="App">
-      {/* <Link to="/"> */}
-      <button className="button">Home</button>
-      {/* </Link> */}
-      {/* <Link to="/about"> */}
-      <button className="button">About</button>
-      {/* </Link> */}
+      <Link to="/">
+        <button className="button">Home</button>
+      </Link>
+      <Link to="/about">
+        <button className="button">About</button>
+      </Link>
     </div>
   );
 };
